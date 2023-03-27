5f84714e011764c67662831b8262c2f90cf138c5
Iaroslav Pripisnov <realiarpri@gmail.com>
2023-03-27 16:02:12 +0300

    add view function $ routes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ai.views import *
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ai/', include('ai.urls'))
 ]
