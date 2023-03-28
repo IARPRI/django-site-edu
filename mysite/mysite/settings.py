5fbcc3293e37970a90a7e7bd5f6b1011a0302161
Iaroslav Pripisnov <realiarpri@gmail.com>
2023-03-28 15:57:07 +0300

    add routes & exceptions

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=x2#zz(tyy^5pgp)*swqize$2#*ng#1+u5lc#8zys_od^=01mc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 
 from ai.views import *
 from django.urls import path, include
@@ -23,3 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ai.urls'))
 ]
+
+handler404 = pageNotFound
