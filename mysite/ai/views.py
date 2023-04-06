367eb81385c4bc84881da205ffa5d1c16e92d9ad
Iaroslav Pripisnov <realiarpri@gmail.com>
2023-04-06 15:40:37 +0300

    upgrade MTV

--- mysite/ai/views.py
+++ mysite/ai/views.py
@@ -10,11 +10,15 @@ menu = [{'title': "About", 'url_name': 'about'},
         ]
 def index(request):
     posts = AI.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Main Page'
+        'title': 'Main Page',
+        'cat_selected': 0,
     }
+
     return render(request, 'ai/index.html', context=context)
 
 def about(request):
@@ -33,4 +37,22 @@ def  pageNotFound (request, exception):
     return HttpResponseNotFound("<h1>This page does not exist</h1>")
 
 def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+    return HttpResponse(f"Show Article with ID = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = AI.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Show by Category',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'ai/index.html', context=context)
